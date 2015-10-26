bc7c6b9f18b1d603faa2a7d6a7e448d75f6fd2f2
Fernando Carrillo <fernando@carrillo.at>
2015-10-26 17:00:33 -0400

    Add partition linked list.

--- src/linkedLists/PartitionLinkedList.java
+++ src/linkedLists/PartitionLinkedList.java
@@ -17,24 +17,53 @@ public class PartitionLinkedList {
 		Node largerHead = null; 
 		Node largerTail = null; 
 		
+		while( n != null ) {
+			Node next = n.next;
+			n.next = null; 
+			if ((Integer) n.data < x) {
+				if(smallerHead == null) { // Add node to empty linked list. 
+					smallerHead =  n; 
+					smallerTail = smallerHead; 
+				} else { // Add node to linked list
+					smallerTail.next = n; 
+					smallerTail = smallerTail.next; 
+				} 
+			} else {
+				if(largerHead == null) { // Add node to empty linked list.
+					largerHead = n; 
+					largerTail = largerHead; 
+				} else {// Add node to linked list
+					largerTail.next = n; 
+					largerTail = largerTail.next; 
+				}
+			}
+			n = next; 
+		}
 		
-		return null; 
+		// Return merged list if both have entries. 
+		if( smallerHead == null ) {
+			return largerHead; 
+		} else {
+			smallerTail.next = largerHead;
+			return smallerHead; 
+		}
 	}
 
 	public static void main(String[] args) {
-		Node n = new Node(1);
-		n.appendToTail(4);
+		Node n = new Node(7);
+		n.appendToTail(1);
 		n.appendToTail(2);
 		n.appendToTail(3);
 		n.appendToTail(4);
 		n.appendToTail(2);
 		n.appendToTail(5);
+		n.appendToTail(2);
 		n.appendToTail(6);
-		n.appendToTail(6);
-		n.appendToTail(7);
+		n.appendToTail(-1);
 		
 		System.out.println(n);
 		
-		Node p = partitionLL(n, 3);  
+		Node p = partitionLL(n, 3);
+		System.out.println(p); 
 	}
 }
